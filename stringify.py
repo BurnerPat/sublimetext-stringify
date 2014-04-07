53fb82b484d120718971c0000f5c6aa84f66319b
BurnerPat <burner.patrick@googlemail.com>
2014-04-07 11:00:34 +0200

    * added de-stringify function for Java

--- stringify.py
+++ stringify.py
@@ -7,3 +7,10 @@ class StringifyJava(sublime_plugin.TextCommand):
 			if not region.empty():
 				text = re.sub(r'([\\"])', r'\\\1', self.view.substr(region))
 				self.view.replace(edit, region, text)
+
+class DeStringifyJava(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for region in self.view.sel():
+			if not region.empty():
+				text = re.sub(r'\\\\', r'\\', self.view.substr(region))
+				self.view.replace(edit, region, text)
